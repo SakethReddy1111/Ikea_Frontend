59d7bf6ea150fe8ff02a96747c2e7b49a264b8a2
SakethReddy1111 <sakethr007@gmail.com>
2022-03-29 16:42:30 +0530

    beggining

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Home } from "./components/Home/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,218 @@
+import { Wrapper } from "./homeCss";
+
+import { Example } from "../carousel/carousel";
+
+export const Home = () => {
+  return (
+    <Wrapper>
+      <div>
+        <a href="/">
+          <img
+            src="https://www.ikea.com/images/enjoy-festive-offers-from-16-march-to-03-april-ec6b7721a119bd5a97806179c045cf53.jpg?f=xxxl"
+            alt=""
+            width={"100%"}
+          />
+        </a>
+      </div>
+      add Here
+      <div id="LimFestColl">
+        <h2>Limited festive collection</h2>
+        <div className="id102">
+          <div className="class102">
+            <h3>AROMATISK</h3>
+            <p>
+              This collection is inspired by the houses, the colourful
+              neighbourhoods in India, and the beautiful art one sees in Indian
+              homes which have a detailed way of storytelling.
+            </p>
+            <a href="/">
+              <img
+                src="https://www.ikea.com/images/3f/37/3f379dc9c27c1fb3ad641abe688a7007.png?f=m"
+                alt=""
+                width={"100%"}
+              />
+            </a>
+          </div>
+
+          <div className="class102">
+            <h3>HEMBJUDEN</h3>
+            <p>
+              This collection is designed to inspire your late-night dinner
+              hosting and renew your home decoration for the festive season.
+            </p>
+            <a href="/">
+              <img
+                src="https://www.ikea.com/images/c5/38/c538994c1701d65d7da457d59a39518b.png?f=m"
+                alt=""
+                width={"100%"}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div id="topCategories">
+        <h2>Top categories</h2>
+      </div>
+      <div id="ikeaFamily">
+        <h2>IKEA Family offers</h2>
+      </div>
+      <hr />
+      <div id="offers">
+        <div className="class103">
+          <a href="/">
+            <img
+              src="https://www.ikea.com/images/ac/97/ac972bdd33a3fdf8244de14bc6365ba2.png?f=xs"
+              alt=""
+              width={"100%"}
+            />
+            <h2>New lower price</h2>
+          </a>
+        </div>
+        <div className="class103">
+          <a href="/">
+            <img
+              src="https://www.ikea.com/images/73/27/73271c15f05d3910dea135c3953729ce.png?f=xs"
+              alt=""
+              width={"100%"}
+            />
+            <h2>Explore top sellers</h2>
+          </a>
+        </div>
+        <div className="class103">
+          <a href="/">
+            <img
+              src="https://www.ikea.com/images/31/5e/315ec0f6b9487c0749056d5df4c04a5e.png?f=xs"
+              alt=""
+              width={"100%"}
+            />
+            <h2>A delicious offer you can't resist!</h2>
+          </a>
+        </div>
+      </div>
+      <hr />
+      <div id="downloadIkea">
+        <div className="class102">
+          <img
+            src="https://www.ikea.com/images/a-hand-showing-ikea-app-download-screen-aec02826f4ecef443ec20186fa64c7d8.jpg?f=m"
+            alt=""
+          />
+        </div>
+
+        <div className="class102">
+          <h2 className="class113">Download the IKEA app</h2>
+          <p>
+            Explore a wide range of affordable, well-designed and functional
+            home furnishing solutions for every room in your home - at your
+            fingertips.
+          </p>
+          <button>Download now</button>
+        </div>
+      </div>
+      <hr />
+      <div id="moresustainable">
+        <h2>Shop products for a more sustainable home</h2>
+        <div></div>
+      </div>
+      <hr />
+      <div>
+        <h2>More ideas and inspiration</h2>
+        <div></div>
+        <div></div>
+      </div>
+      <hr />
+      <div>
+        <div>
+          <h3>Returns policy</h3>
+          <p>Exceptions during COVID19</p>
+          <a href="/">Learn more</a>
+        </div>
+        <div>
+          <h3>Click & collect</h3>
+          <p>Safe click and collect shopping at IKEA Stores!</p>
+          <a href="/">Learn more</a>
+        </div>
+        <div>
+          <h3>Planning tools</h3>
+          <p>Our planning tools will help you give shape to your ideas</p>
+          <a href="/">Learn more</a>
+        </div>
+        <div>
+          <h3>Safety at IKEA store</h3>
+          <p>Safe shopping experience at IKEA</p>
+          <a href="/">Learn more</a>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <h2>Important information</h2>
+        <div>
+          <div>
+            <h3>Coronavirus (COVID-19) information</h3>
+            <a href="/">
+              <img
+                src="https://www.ikea.com/images/67/4d/674d38ec377caa8b14da0718ae34638e.jpg?f=m"
+                alt=""
+              />
+              <button>Learn more</button>
+            </a>
+          </div>
+          <div>
+            <h3>Product recalls</h3>
+            <a href="/">
+              <img
+                src="https://www.ikea.com/images/bf/e2/bfe239c44b15ce483c987e2250df4d64.jpg?f=m"
+                alt=""
+              />
+              <button>Learn more</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <h2>Explore our furniture & home furnishing range</h2>
+        <div>
+          <div>
+            <div>
+              <h3>IKEA is a global leader in life at home.</h3>
+              <p>
+                Whether you just moved into a new home or looking to revamp your
+                current one, we at IKEA are here to inspire you with affordable
+                home furniture solutions, there is a piece of furniture to every
+                corner of your home. Create a home that is perfect for you.
+              </p>
+              <p>
+                Shopping at IKEA is a bit different and exciting compared to
+                your shopping at an everyday retail. It is about experiencing
+                solutions first hand and getting to know ideas and inspirations
+                that can fit perfectly into your home. That’s why, we offer more
+                than 7500 products, solutions at our store along with home
+                furnishing ideas and services for you to explore.
+              </p>
+              <p>
+                When you visit IKEA store, make yourself at home in our many
+                room settings in the store. Squeeze the upholsteries, feel the
+                oriental rugs, try the sofa beds and open the cabinets to feel
+                the quality. On the price tag, you’ll find all you need to know
+                about a product, including where in the store you can pick it
+                up.
+              </p>
+              <p>
+                Since most IKEA furniture is flat-packed, they are quite easy to
+                bring home when you buy from the store.
+              </p>
+            </div>
+            <div>
+              <a href="/">
+                Sofas | Beds | Dining | Tv units | Mattresses | Seating | Coffee
+                tables | Wardrobes | Storage | Bookshelves | Shoe racks | Decor|
+                Bathroom | Textiles | Pots & plants | Home electronics | Home
+                improvement | Lighting
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
