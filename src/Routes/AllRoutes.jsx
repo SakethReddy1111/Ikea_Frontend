6092fb13d83eb8ff1f23299e2ff2818f15edc1c9
vattsalbhatt <vatsbhatt333@gmail.com>
2022-04-01 19:04:35 +0530

    Signup and Login auth

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router";
 import { AddProduct } from "../Components/AddProducts/AddProduct";
+import { Login } from "../Components/Login/Login";
+import { LoginWrapper } from "../Components/Login/LoginWrapper";
 import { Register } from "../Components/SignUp/Register";
 import { SignUp } from "../Components/SignUp/Signup";
 
@@ -8,7 +10,8 @@ export const AllRoutes = () => {
     <>
       <Routes>
         <Route path="/addproduct" element={<AddProduct />} />
-        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
